3f1f64e24187007e4763084c4806f1fd1b2db97d
sailorfe macos <sailorfe@proton.me>
2024-01-03 14:57:48 -0500

    input mode works on mac, not tested lnx

--- keyboards/planck/rev7/keymaps/astro-code/keymap.c
+++ keyboards/planck/rev7/keymaps/astro-code/keymap.c
@@ -10,6 +10,7 @@ enum planck_keycodes { QWERTY = SAFE_RANGE };
 #define PAD     MO(_NUMPAD)
 #define ADJUST  MO(_ADJUST)
 #define CELLO   TG(_CELLO)
+#define UTF     MO(_UNICODE)
 #define ALT_SFT RALT(KC_RSFT)
 #define GUI_SFT RGUI(KC_RSFT)
 #define CTL_SFT RCTL(KC_RSFT)
@@ -57,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
         KC_ESC, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
         KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_ENT,
-        PAD,    KC_LCTL,KC_LGUI,KC_LALT,LOWER,  VIM,    KC_SPC, RAISE,  ALT_SFT,GUI_SFT,CTL_SFT,CW_TOGG
+        PAD,    KC_LCTL,KC_LGUI,KC_LALT,LOWER,  VIM,    KC_SPC, RAISE,  UTF,    GUI_SFT,CTL_SFT,CW_TOGG
     ),
     [_LOWER] = LAYOUT_planck_grid(
         KC_TILD,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,
